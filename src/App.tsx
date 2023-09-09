d072787c532865d7786c838372b4906ceb7644f0
manish774 <manishranbir774@gmail.com>
2023-09-09 11:58:28 +0530

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,52 @@
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+// import TextBlock from './textBlock';
 import "./App.css";
-import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Parallax pages={2} style={{ top: "0", left: "0" }} className="animation">
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div className="animation_layer parallax" id="artback"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div className="animation_layer parallax" id="mountain"></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={-0.1}>
+          <div
+            className="animation_layer parallax"
+            style={{
+              textAlign: "center",
+              color: "#FF7722",
+              fontSize: "40px",
+              fontWeight: "bolder",
+              top: "25%",
+            }}
+          >
+            ॐ क्रीं कालिकायै नमः
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div className="animation_layer parallax" id="jungle1"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div className="animation_layer parallax" id="jungle2"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div className="animation_layer parallax" id="jungle3"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.45}>
+          <div className="animation_layer parallax" id="jungle4"></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div className="animation_layer parallax" id="jungle5"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.25}>
+          {/* <TextBlock /> */}
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import img from "../../Assets/baglamukhi-devi-ki-photo-1920x1080.jpg";
-import classes from "./Home.module.scss";
+import "./Home.css";
 const Home = () => {
   return (
     <div>
-      <img src={img} className={classes.img} />
+      <div className="parallax-container">
+        <div className="parallax-content">
+          <h1>Welcome to My Parallax Website</h1>
+          <p>Scroll down to see the parallax effect</p>
+        </div>
+        <div className="parallax-image">
+          <img src={img} alt="Background" />
+        </div>
+        <div className="parallax-overlay">
+          <img src={img} alt="Overlay" />
+        </div>
+      </div>
     </div>
   );
 };
